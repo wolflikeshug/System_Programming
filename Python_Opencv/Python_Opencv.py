32ae78d0d1dffac04649abb5520119102758eda3
wolf_likes_hug <huzhuodaniel@gmail.com>
2022-08-25 02:23:11 +0000

    update

--- Python_Opencv/Python_Opencv.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-import cv2
-
-img = cv2.imread("C:\\Users\\HuggingWolf\\OneDrive\ͼƬ\\Saved Pictures\\3a4551e0da174e7bacb5f2c3f6776f6b.png")
-cv2.imshow("Kali", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
