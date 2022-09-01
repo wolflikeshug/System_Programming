247a132731f75406cbba74593a3001c04689d5fa
wolf_likes_hug <huzhuodaniel@gmail.com>
2022-09-01 00:49:44 +0000

    update

--- Lab3/lecture9.c
+++ Lab3/lecture9.c
@@ -4,13 +4,22 @@
 
 void main(void)
 {
-int g = 3;
-switch (g%2)
-{
-case 1:
-    printf("%d\n", g);;
-case 0:
-    printf("%d\n", g);
+int N = 5;
+int A[N+1][N+1];
+for(int j=0 ; j<N ; ++j) {
+for(int i=0 ; i<N ; ++i) {
+if((i%N) == j)
+A[j][i] = 1;
+else
+A[j][i] = 0;
 
 }
 }
+for(int j=0 ; j<N ; ++j) {
+for(int i=0 ; i<N ; ++i) {
+printf("%i ", A[i][j]);
+}
+printf("\n");
+}
+}
+
